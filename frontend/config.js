f6d56658d1b32c1f78ad917edf35957c9a7ba272
675432100641 <Woravit@Admins-iMac.local>
2026-03-18 13:16:56 +0700

    fix : config URL

---
+++ frontend/config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  AUTH_URL:     'https://auth-service-production-f062.up.railway.app',
+  TASK_URL:     'https://your-task-service.up.railway.app',
+  ACTIVITY_URL: 'https://your-activity-service.up.railway.app'
+};
