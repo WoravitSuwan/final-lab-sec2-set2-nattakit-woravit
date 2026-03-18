6cc1d98902f58338a460f4326fc2543ed872e61a
WoravitSuwan <Spacemanc1105@gmail.com>
2026-03-18 16:57:49 +0700

    fix: db activityservice

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   AUTH_URL:     'https://auth-service-production-f062.up.railway.app',
   TASK_URL:     'https://task-service-production-557c.up.railway.app',
-  ACTIVITY_URL: 'https://activity-service-production-57f5.up.railway.app'
+  ACTIVITY_URL: 'https://activity-service-production-d849.up.railway.app'
 };
